c4c4f372fade475777479822dd66ef0526e9d2ee
Octavian Drulea <odrulea@cpf.com>
2014-04-30 21:19:18 -0700

    company is now added when you sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  #attr_accessible :company_attributes
+
   belongs_to :company
+  accepts_nested_attributes_for :company
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
