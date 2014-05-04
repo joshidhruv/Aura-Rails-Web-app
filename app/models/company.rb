ea8d4a3088a0b2d60d8b6cf345fedb71e945be60
amandamcwhorter <amanda.mcwhorter@gmail.com>
2014-05-04 10:40:48 -0700

    fixed conflicts

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,5 @@
 class Company < ActiveRecord::Base
   has_many :users
-<<<<<<< HEAD
   has_many :locations
-=======
   validates :name, :presence => true
->>>>>>> cbb2778275473f7a1e09403237086d26e835fa94
 end
